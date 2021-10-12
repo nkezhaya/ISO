8a77e5a6e24bb0fd3125dc8acf7ede001312e88a
Nick Kezhaya <nick@whitepaperclip.com>
2021-10-12 07:57:09 -0500

    1.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.2.0
+
+  * Fixed some subdivision search issues by adding equal match checks
+
 ## 1.1.0
 
   * Added trigram search for `find_subdivision/2`
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ISO.MixProject do
   def project do
     [
       app: :iso,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       description: description(),
